9b3c45ff21cd9e51ca9a3396a4e25b743a6d539c
awe peter <awe.petersq@gmail.com>
2022-03-18 15:22:37 -0700

    crypto news

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cryptoApi } from "../components/services/cryptoApi";
+import { cryptoNewsApi } from "../components/services/cryptoNewsApi";
 
 export default configureStore({
     reducer: {
         [cryptoApi.reducerPath]: cryptoApi.reducer,
+        [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
     }, 
 });
